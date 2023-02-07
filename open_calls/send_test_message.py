11971fc2d3fe29d71a6254506e9a9dc18a504e26
dennis-lai96 <dennis.lai96@gmail.com>
2023-02-06 16:26:05 -0800

    changed msg receiver
    hopefully you receive a message now :')

--- open_calls/send_test_message.py
+++ open_calls/send_test_message.py
@@ -11,6 +11,6 @@ def handle_request():
     message = g.sms_client.messages.create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
                      from_=yml_configs['twillio']['phone_number'],
-                     to='+18584423590'
+                     to=message['From']
                  )
     return json_response( sid = message.sid )
