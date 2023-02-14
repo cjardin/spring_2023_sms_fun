b787610a6bdf7894a1df16fde632f3944d374615
JoshyLar <113719434+JoshyLar@users.noreply.github.com>
2023-02-13 20:57:44 -0800

    Create ntlmod.py
    modified the code from class to accept an arg and return the results from both nltk funcs

---
+++ ntlmod.py
@@ -0,0 +1,16 @@
+import nltk
+#nltk.download('vader_lexicon')
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('punkt')
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def analyzeSentiment(string):
+
+    sia = SentimentIntensityAnalyzer()
+    #print(sia.polarity_scores('Wow. NLTK IS SICK!'))
+    polarity = sia.polarity_scores(string)
+    pos= nltk.pos_tag(nltk.word_tokenize(string))
+
+    return string, polarity, pos
+
+print(analyzeSentiment('yummy yummy! I love python in my tummy'))
