5de72284affedd3536f659e7fc9381aabd336dc5
dennis-lai96 <dennis.lai96@gmail.com>
2023-02-14 16:15:06 -0800

    added first cellmon, can just spam the shit out of the rest now

--- gameclass.py
+++ gameclass.py
@@ -7,51 +7,58 @@ class Player:
 
 class Cellmon:
 
-    def __init__(self, level, baseAttack=5, baseDefense=2, baseSpDef=1, baseSpeed=3, baseHP=2, baseSpAttack=4):
+    def __init__(self, level, baseAttack=0, baseDefense=0, baseSpDef=0, baseSpeed=0, baseHP=0, baseSpAttack=0, species ="Cellmon"):
+        self.species = species
         self.level = level
-        self.maxHP = baseHP*level
-        self.speed = baseSpeed*level
-        self.spDef = baseSpDef*level
-        self.defense = baseDefense*level
-        self.attack = baseAttack*level
-        self.spAttack = baseSpAttack*level
+        self.maxHP = baseHP * level
+        self.speed = baseSpeed * level
+        self.spDef = baseSpDef * level
+        self.defense = baseDefense * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
 
-
-        self.baseHP= baseHP
-        self.currentHP=baseHP
-        self.baseAttack=baseAttack
-        self.baseDefense=baseDefense
-        self.baseSpeed=baseSpeed
-        self.baseSpAttack=baseSpAttack
-        self.baseSpDef=baseSpDef
-
-
-
+        self.baseHP = baseHP
+        self.currentHP = baseHP
+        self.baseAttack = baseAttack
+        self.baseDefense = baseDefense
+        self.baseSpeed = baseSpeed
+        self.baseSpAttack = baseSpAttack
+        self.baseSpDef = baseSpDef
 
     def level_up(self):
         print("You've leveled up! ")
         self.maxHP = self.maxHP + self.baseHP
-        self.attack = self.attack+ self.baseAttack
+        self.attack = self.attack + self.baseAttack
         self.defense = self.defense + self.baseDefense
         self.spAttack = self.spAttack + self.baseSpAttack
         self.spDef = self.spDef + self.baseSpDef
         self.speed = self.speed + self.baseSpeed
-        self.level+=1
+        self.level += 1
         self.printMaxStats()
 
-
     def printMaxStats(self):
-        print("Level: ", self.level, "\nMaximum HP: ", self.maxHP, "\nAttack: ",self.attack,"\nDefense: ",self.defense, "\nSpAttack: ", self.spAttack)
-        print("SpDef: ",self.spDef,"\nSpeed: ",self.speed)
+        print("Species: ",self.species, "\nLevel: ", self.level, "\nMaximum HP: ", self.maxHP, "\nAttack: ", self.attack, "\nDefense: ",
+              self.defense, "\nSpAttack: ", self.spAttack)
+        print("SpDef: ", self.spDef, "\nSpeed: ", self.speed)
+
 
 class Aichu(Cellmon):
-    baseHP = 10
-    baseAtck = 3
-    baseSpAtck = 4
-    baseDef = 3
-    baseSpDef = 3
-    baseSpeed = 3
-
+    def __init__(self, level, baseAttack=4, baseDefense=2, baseSpDef=3, baseSpeed=5, baseHP=10, baseSpAttack=12, species = "Aichu"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.speed = baseSpeed * level
+        self.spDef = baseSpDef * level
+        self.defense = baseDefense * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.baseHP = baseHP
+        self.currentHP = baseHP
+        self.baseAttack = baseAttack
+        self.baseDefense = baseDefense
+        self.baseSpeed = baseSpeed
+        self.baseSpAttack = baseSpAttack
+        self.baseSpDef = baseSpDef
 
 
 
@@ -61,10 +68,10 @@ class Location:
         Area.mobs = mobs[:4]
 
 
-
-
-cellmon =  Cellmon(1)
+cellmon = Cellmon(1)
 cellmon.printMaxStats()
 cellmon.level_up()
 
-
+pikablu = Aichu(1)
+pikablu.printMaxStats()
+pikablu.level_up()
