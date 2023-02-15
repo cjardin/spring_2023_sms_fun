e5c7f0ca66c863335462a58a292ed8ab0a27c677
dennis-lai96 <dennis.lai96@gmail.com>
2023-02-14 16:35:44 -0800

    Cleaned up stat order so its not just a haphazard mess

--- gameclass.py
+++ gameclass.py
@@ -7,30 +7,31 @@ class Player:
 
 class Cellmon:
 
-    def __init__(self, level, baseAttack=0, baseDefense=0, baseSpDef=0, baseSpeed=0, baseHP=0, baseSpAttack=0, species ="Cellmon"):
+    def __init__(self, level, baseHP=0, baseAttack=0, baseSpAttack=0, baseDefense=0, baseSpDef=0, baseSpeed=0, species ="Cellmon"):
         self.species = species
         self.level = level
         self.maxHP = baseHP * level
-        self.speed = baseSpeed * level
-        self.spDef = baseSpDef * level
-        self.defense = baseDefense * level
         self.attack = baseAttack * level
         self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
 
         self.baseHP = baseHP
-        self.currentHP = baseHP
+        self.currentHP = self.maxHP
         self.baseAttack = baseAttack
-        self.baseDefense = baseDefense
-        self.baseSpeed = baseSpeed
         self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
         self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
 
     def level_up(self):
         print("You've leveled up! ")
         self.maxHP = self.maxHP + self.baseHP
         self.attack = self.attack + self.baseAttack
-        self.defense = self.defense + self.baseDefense
         self.spAttack = self.spAttack + self.baseSpAttack
+        self.defense = self.defense + self.baseDefense
         self.spDef = self.spDef + self.baseSpDef
         self.speed = self.speed + self.baseSpeed
         self.level += 1
@@ -43,22 +44,26 @@ class Cellmon:
 
 
 class Aichu(Cellmon):
-    def __init__(self, level, baseAttack=4, baseDefense=2, baseSpDef=3, baseSpeed=5, baseHP=10, baseSpAttack=12, species = "Aichu"):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Aichu"):
         self.species = species
         self.level = level
         self.maxHP = baseHP * level
-        self.speed = baseSpeed * level
-        self.spDef = baseSpDef * level
-        self.defense = baseDefense * level
         self.attack = baseAttack * level
         self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
         self.baseHP = baseHP
-        self.currentHP = baseHP
+        self.currentHP = self.maxHP
         self.baseAttack = baseAttack
-        self.baseDefense = baseDefense
-        self.baseSpeed = baseSpeed
         self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
         self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
+
+
 
 
 
