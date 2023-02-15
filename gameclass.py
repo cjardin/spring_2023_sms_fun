adaa58ca1f200f1ede0a4bed64fbe663d362d0e7
dennis-lai96 <dennis.lai96@gmail.com>
2023-02-14 18:34:08 -0800

    Cleaned up stat order so its not just a haphazard mess

--- gameclass.py
+++ gameclass.py
@@ -63,9 +63,221 @@ class Aichu(Cellmon):
         self.baseSpDef = baseSpDef
         self.baseSpeed = baseSpeed
 
+class Terrasaur(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Terrasaur"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
 
 
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
 
+class Verizard(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Verizard"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
+
+class Gekkip(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Gekkip"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
+
+class Capybrawla(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Capybrawla"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
+
+class Beesiege(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Beesiege"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
+
+class Jellyfists(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Jellyfists"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
+
+class Doomosaur(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Doomosaur"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
+
+class Parsnipe(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Doomosaur"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+        self.baseSpeed = baseSpeed
+
+class Pandamonium(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Pandamonium"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+
+class Fiamelon(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Fiamelon"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
+class Armordillo(Cellmon):
+    def __init__(self, level, baseHP=3, baseAttack=4, baseSpAttack=5, baseDefense=2, baseSpDef=6, baseSpeed=4, species = "Fiamelon"):
+        self.species = species
+        self.level = level
+        self.maxHP = baseHP * level
+        self.attack = baseAttack * level
+        self.spAttack = baseSpAttack * level
+        self.defense = baseDefense * level
+        self.spDef = baseSpDef * level
+        self.speed = baseSpeed * level
+
+
+        self.baseHP = baseHP
+        self.currentHP = self.maxHP
+        self.baseAttack = baseAttack
+        self.baseSpAttack = baseSpAttack
+        self.baseDefense = baseDefense
+        self.baseSpDef = baseSpDef
 
 class Location:
     def __init__(Area, Aname, mobs):
