f3cd5b54e8184d929f31bccc7dac10c96064e8ba
huynh-kent <huynhkentd@gmail.com>
2023-02-16 14:04:55 -0800

    newer messages now appear at beginning of list

--- actors.py
+++ actors.py
@@ -2,6 +2,7 @@ class actor:
     def __init__(self, phone_number):
         self.phone = phone_number
         self.prev_msgs = []
+        self.name = ''
 
     def save_msg(self, msg):
-        self.prev_msgs.append(msg)
+        self.prev_msgs.insert(0, msg)
