cbbe35e8c186637d3b957adbfb1341986b47938e
huynh-kent <huynhkentd@gmail.com>
2023-02-21 13:45:56 -0800

    moved processing incoming messages to here

--- processing_message.py
+++ processing_message.py
@@ -1,3 +1,15 @@
+import random
+import json
+
+# open corpus json
+CORPUS = {}
+with open('chatbot_corpus.json', 'r') as myfile:
+    CORPUS = json.loads(myfile.read())
 
 def process_message(sent_input):
-    pass
+    if sent_input in CORPUS['input']:
+        response = random.choice(CORPUS['input'][sent_input])
+    else:
+        CORPUS['input'][sent_input] = ['DID NOT FIND']
+        with open('chatbot_corpus.json', 'w') as myfile:
+            myfile.write(json.dumps(CORPUS, indent=4 ))
