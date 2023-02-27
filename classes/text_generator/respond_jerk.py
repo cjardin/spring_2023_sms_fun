096e052d0cd21c02fe7a415e1f42ffb88b02ce8a
Michael Andrews <72107310+mandrewspicard@users.noreply.github.com>
2023-02-26 22:13:34 -0800

    More phrases

--- classes/text_generator/respond_jerk.py
+++ classes/text_generator/respond_jerk.py
@@ -18,33 +18,36 @@ class JerkResponder(TextGenerator):
         return self.response
 
     def rate(self, in_text: ProcessedText) -> float:
-        curPos = 0
         responses = []
         # Generate simple responses based on keywords
 
         for input_word, input_tag in zip(in_text.words, in_text.tags):
-            if input_tag == 'VBD':
-                responses.append((0.8 + random(), "I " + input_word + " your mom last night"))
-                # responses.append((0.8 + random(), "I " + input_word + " your mom last night"))
-                # responses.append((0.8 + random(), "I " + input_word + " your mom last night"))
-                # responses.append((0.8 + random(), "I " + input_word + " your mom last night"))
+            if input_tag == 'VBD':  # Past Tense Verbs
+                responses.append((0.30 + random(), "I " + input_word + " your mom last night"))
+                responses.append((0.30 + random(), "When was the last time you " + input_word + "?"))
 
-            if input_tag == 'NNS':
-                responses.append((
-                    0.5 + random(),
-                    "Man I *love* talking about " + input_word
-                ))
-                responses.append((
-                    0.5 + random(),
-                    "Do you really care about " + input_word + "?  That's kinda weird man"
-                ))
+            if input_tag == 'NNS':  # Plural Nouns
+                responses.append((0.50 + random(), "Man I *love* talking about " + input_word))
+                responses.append((0.50 + random(), "Do you really care about " + input_word + "?  That's kinda weird man"))
 
-            if input_tag == 'NN':
-                responses.append((
-                    0.25 + random(),
-                    input_word + "!? A " + input_word + " killed my entire family"))
+            if input_tag == 'NP':  # Proper Nouns
+                responses.append((0.50 + random(), "Wait, *the* " + input_word + "?  Holy shit!"))
+                responses.append((0.10 + random(), "Yo, didn't " + input_word + "get cancelled?"))
+                responses.append((0.25 + random(), input_word + "? From OnlyFans?"))
 
-            curPos += 1
+            if input_tag == 'NNS':  # Present Participle (-ing)
+                responses.append((0.30 + random(), input_word + "? Man you're optimistic!"))
+
+            if input_tag == 'NN':  # Singular Nouns
+                responses.append((0.25 + random(), input_word + "!? A " + input_word + " killed my entire family"))
+                responses.append((0.10 + random(), "Do you really love the " + input_word + " or are you just naming things in the room?"))
+                responses.append((0.60 + random(), "Please tell me more about this" + input_word + ".  I am sooo interested -_-"))
+
+            if input_tag == 'JJR':  # Comparative adjectives (-er)
+                responses.append((0.55 + random(), input_word + "? I hardly know 'er!"))
+
+            if input_tag == 'JJS':  # Superlative adjectives (-est)
+                responses.append((0.25 + random(), "The " + input_word + ", huh?  Have you checked every one?"))
 
         if responses:
             (weight, self.response) = max(responses)
